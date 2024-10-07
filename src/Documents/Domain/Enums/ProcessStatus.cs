7f19c44c30770829074a65949169a730fcae555f
Nishudar <12666332+nishudar@users.noreply.github.com>
2024-10-08 01:01:05 +0200

    fix bug with abandoning

--- src/Documents/Domain/Enums/ProcessStatus.cs
+++ src/Documents/Domain/Enums/ProcessStatus.cs
@@ -4,7 +4,7 @@ public static class ProcessStatus
 {
     public const string Started = "started";
     public const string Finished = "finished";
-    public const string Abandoned = "finished";
+    public const string Abandoned = "abandoned";
     
     public static string[] ProcessStatuses => [Started, Finished, Abandoned];
 }
